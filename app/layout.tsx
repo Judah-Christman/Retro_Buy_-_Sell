1ff0196a9e528e8c73674a532e3dfd29a258bb8d
Judah-Christman <judah.christman@gmail.com>
2025-07-11 00:30:12 -0700

    adding content

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Footer } from "/components";
+import { Footer } from "./components";
 
 export const metadata: Metadata = {
   title: "Create Next App",
