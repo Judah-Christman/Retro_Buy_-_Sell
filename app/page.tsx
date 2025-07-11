b18de9ad5f652b7a8ed645fd04c9286f85bee99d
Judah-Christman <judah.christman@gmail.com>
2025-07-11 00:31:29 -0700

    adding content

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Navigation, About, Reviews, Contact } from './components.tsx';
+import { Navigation, About, Reviews, Contact } from './components';
 
 export default function Home() {
   return (
