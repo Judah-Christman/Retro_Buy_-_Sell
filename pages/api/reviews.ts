5eeb29dfeba8fc5c18ce267e3d6de41f68647442
Judah-Christman <judah.christman@gmail.com>
2025-07-11 08:20:44 -0700

    adding content

--- pages/api/reviews.ts
+++ pages/api/reviews.ts
@@ -1,4 +1,4 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from "next/types";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const url =
